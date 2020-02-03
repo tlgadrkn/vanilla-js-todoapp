ad80ca7109396e0f94b839bb1b4be8c2186ebdcd
tlgadrkn <tolgadurukan16@gmail.com>
2020-02-04 00:32:13 +0200

    Switching to  localStorage

--- app.js
+++ app.js
@@ -1,51 +1,66 @@
-let todos = [];
-
-// FUNCTION get todos - from local storage
-
+const LOCAL_STORAGE_KEY_NAME = 'item';
 const form = document.querySelector('form');
 const todoContent = document.querySelector('#todoContent');
+let todos = getTodos(LOCAL_STORAGE_KEY_NAME);
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const todoInput = document.querySelector('#todoInput');
-    todoInput.value === "" || todoInput.value == undefined ? alert('Please enter a task') : addTodo(todoInput.value);
-    todoInput.value = "";
-    render(todos)        
+// get todos from local Storage
+function getTodos(key) {
+    let todos = localStorage.getItem(key);
+    Array.isArray(todos) && todos.length || todos !== null ? todos = JSON.parse(todos) : todos = [];
+    return todos;
+}
 
-})
+// create todo //
 
+// const createTodo = todoTitle => {
+//     let tempTodo = {
+//         id: Math.floor(Math.random() * 9999), // Normally I would use uuid etc.
+//         title: todoTitle,
+//         completed: false
+//     }
+//     return tempTodo
+// }
 
-const addTodo = todo => {
+// todos to local storage
+const addTodo = todoTitle => {
     let tempTodo = {
-        id: Math.floor(Math.random() * 9999), // Normally I would use uuid etc.
-        title: todo,
-        completed: false
-    }
-
+                id: Math.floor(Math.random() * 9999), // Normally I would use uuid etc.
+                title: todoTitle,
+                completed: false
+            }
     todos.push(tempTodo);
-    console.log(todos);
+    localStorage.setItem(LOCAL_STORAGE_KEY_NAME, JSON.stringify(todos))
+    console.log(JSON.stringify(tempTodo));
+    return tempTodo
 
 }
 
+//delete todos from local storage and browser
 const deleteTodo = (e) => {
 
     if (e.target.getAttribute('id') === 'deleteTodo') {
-        const parentElement = e.target.parentElement; 
-        todos = [...todos].filter( todo => todo.id == parentElement.getAttribute('id') ? parentElement.remove() : todo);
-        render(todos);
-        console.log(todos);
+        const parentElement = e.target.parentElement;
+
+         //first remove element from DOM
+         todos = todos.filter(todo => todo.id == parentElement.getAttribute('id') ? parentElement.remove() : todo);
+
+        //then remove todo in localStorage
+        let storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NAME))
+        let filteredTodos = storedTodos.filter( todo => todo.id != parentElement.getAttribute('id'));
+        localStorage.clear();
+
+        addTodo(filteredTodos) 
+        // render(todos);
 
     }
-    
+
 }
 
-document.querySelector('#todoContent').addEventListener('click', deleteTodo);
-console.log(todos);
-
 const render = (todos) => {
-
-    let allTodos = todos.map(todo => {
+        
+    
+    let allTodos =  todos.map(todo => {
         if (todo.completed) {
             return `<li id=${todo.id} class="list-group-item">${todo.title}</li>`
 
@@ -57,10 +72,22 @@ const render = (todos) => {
 
         }
     })
+
     todoContent.innerHTML = allTodos.join("")
-    console.log(todos.length)
+    // console.log(todos)
 
 }
-// document.addEventListener('DOMContentLoaded', renderTodos())
-render(todos)
+document.addEventListener('DOMContentLoaded', getTodos(LOCAL_STORAGE_KEY_NAME))
+document.addEventListener('DOMContentLoaded', render(todos))
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const todoInput = document.querySelector('#todoInput');
+    todoInput.value === "" || todoInput.value == undefined ? alert('Please enter a task') : addTodo(todoInput.value);
+    todoInput.value = "";
+    render(todos)
+
+})
+
+
+document.querySelector('#todoContent').addEventListener('click', deleteTodo);
